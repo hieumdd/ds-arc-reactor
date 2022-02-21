ad0a6f145fbf0e5e6de9c29921dfd00d9eedeaa2
HM <hieumdd@gmail.com>
2022-02-22 00:39:45 +0700

    feat: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,139 @@
+import * as dscc from '@google/dscc';
+
+import * as Chart from 'chart.js';
+import 'chartjs-plugin-piechart-outlabels';
+
+import * as d3Interpolate from 'd3-interpolate';
+
+import zip from 'lodash-es/zip';
+
+import * as local from './localMessage';
+
+type Field = {
+    id: string;
+    name: string;
+    type: string;
+    concept: string;
+};
+
+type Data = {
+    tables: {
+        DEFAULT: {
+            dimID: string[];
+            metricID: (number | null)[];
+        }[];
+    };
+    fields: {
+        dimID: Field[];
+        metricID: Field[];
+    };
+};
+
+const margin = {
+    top: 10,
+    bottom: 10,
+    right: 10,
+    left: 10,
+};
+
+const height = dscc.getHeight() - margin.top - margin.bottom;
+const width = dscc.getWidth() - margin.left - margin.right;
+
+const canvasElement = document.createElement('canvas');
+canvasElement.id = 'chart';
+canvasElement.height = height;
+canvasElement.width = width;
+document.body.appendChild(canvasElement);
+
+const lowestHex = '#ffc3c5';
+const lowestRGB = 'rgb(255, 195, 197)';
+const highestHex = '#ff0008';
+const interpolator = d3Interpolate.interpolateRgb(lowestHex, highestHex);
+
+const itp = (values: number[]) => {
+    const max = Math.max(...values);
+    const min = Math.min(...values);
+    const normalized = values.map((val) => (val - min) / (max - min));
+    return normalized.map((val) => interpolator(val));
+};
+
+const drawViz = (data: Data) => {
+    const ctx = canvasElement.getContext('2d');
+
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    const metricValue = zip(
+        ...data.tables.DEFAULT.map(({ metricID }) => metricID),
+    );
+    const metricKey = data.fields.metricID.map(({ name }) => name);
+
+    const datasets = zip(metricKey, metricValue).map(
+        ([label, actualData]: [string, number[]], i: number) => ({
+            label,
+            actualData,
+            data: actualData.map(() => 1),
+            backgroundColor: actualData.every(
+                (i: number) => i === 0 || i === undefined || i === null,
+            )
+                ? actualData.map(() => lowestRGB)
+                : itp(actualData),
+            borderWidth: 5,
+            outlabels: {
+                display: i === 0,
+                text: '%l',
+                color: 'black',
+                backgroundColor: 'white',
+                stretch: 45,
+                font: {
+                    resizable: true,
+                    minSize: 12,
+                    maxSize: 18,
+                },
+            },
+        }),
+    );
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: data.tables.DEFAULT.map(({ dimID }) => dimID).map(
+                ([dimID]) => dimID,
+            ),
+            datasets,
+        },
+        options: {
+            cutoutPercentage: 40,
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 50,
+                    bottom: 50,
+                },
+            },
+            legend: {
+                display: false,
+            },
+            tooltips: {
+                mode: 'nearest',
+                intersect: true,
+                callbacks: {
+                    label({ datasetIndex, index }) {
+                        const { label } = datasets[datasetIndex];
+                        const value = datasets[datasetIndex].actualData[index];
+                        return `${label}: ${value}`;
+                    },
+                },
+            },
+        },
+    });
+};
+
+const LOCAL = true;
+
+if (LOCAL) {
+    drawViz(local.message);
+} else {
+    // @ts-expect-error: Custom Type
+    dscc.subscribeToData(drawViz, { transform: dscc.objectTransform });
+}
---
+++ src/localMessage.ts
@@ -0,0 +1,265 @@
+export const message = {
+    tables: {
+        DEFAULT: [
+            {
+                dimID: ['Email'],
+                metricID: [0, 16, 24, 32, 16, 10],
+            },
+            {
+                dimID: ['SMS'],
+                metricID: [4, 26, 28, 17, 20, 20],
+            },
+            {
+                dimID: ['Google'],
+                metricID: [8, 15, 30, 22, 20, 50],
+            },
+            {
+                dimID: ['Facebook'],
+                metricID: [12, 15, 27, 23, 16, 80],
+            },
+            {
+                dimID: ['Email'],
+                metricID: [16, 16, 24, 32, 16, 100],
+            },
+            {
+                dimID: ['SMS'],
+                metricID: [20, 26, 28, 17, undefined, 70],
+            },
+            {
+                dimID: ['Google'],
+                metricID: [24, 15, 30, 22, 20, 70],
+            },
+            {
+                dimID: ['Facebook'],
+                metricID: [28, 15, 27, 23, 16, 70],
+            },
+        ],
+    },
+    fields: {
+        dimID: [
+            {
+                id: 'qt_y5e3muxxlc',
+                name: 'source',
+                type: 'TEXT',
+                concept: 'DIMENSION',
+            },
+        ],
+        metricID: [
+            {
+                id: 'qt_z5e3muxxlc',
+                name: 'impressions',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+            {
+                id: 'qt_sv79zuxxlc',
+                name: 'isqls',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+            {
+                id: 'qt_xvir1uxxlc',
+                name: 'msqls',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+            {
+                id: 'qt_y28a3uxxlc',
+                name: 'sqls',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+            {
+                id: 'qt_zz1h4uxxlc',
+                name: 'closed_won',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+            {
+                id: 'qt_zz12323uxxlc',
+                name: 'super',
+                type: 'NUMBER',
+                concept: 'METRIC',
+            },
+        ],
+    },
+    style: {},
+    theme: {
+        themeFillColor: {
+            color: '#ffffff',
+            themeRef: {
+                index: 0,
+            },
+        },
+        themeFontColor: {
+            color: '#000000',
+            themeRef: {
+                index: 1,
+            },
+        },
+        themeFontFamily: 'Roboto',
+        themeAccentFillColor: {
+            color: '#e0e0e0',
+            themeRef: {
+                index: 2,
+            },
+        },
+        themeAccentFontColor: {
+            color: '#000000',
+            themeRef: {
+                index: 3,
+            },
+        },
+        themeAccentFontFamily: 'Roboto',
+        themeSeriesColor: [
+            {
+                color: '#0072f0',
+                seriesRef: {
+                    index: 0,
+                },
+                themeRef: {
+                    index: 4,
+                },
+            },
+            {
+                color: '#00b6cb',
+                seriesRef: {
+                    index: 1,
+                },
+                themeRef: {
+                    index: 5,
+                },
+            },
+            {
+                color: '#f10096',
+                seriesRef: {
+                    index: 2,
+                },
+                themeRef: {
+                    index: 6,
+                },
+            },
+            {
+                color: '#f66d00',
+                seriesRef: {
+                    index: 3,
+                },
+                themeRef: {
+                    index: 7,
+                },
+            },
+            {
+                color: '#ffa800',
+                seriesRef: {
+                    index: 4,
+                },
+                themeRef: {
+                    index: 8,
+                },
+            },
+            {
+                color: '#7cb342',
+                seriesRef: {
+                    index: 5,
+                },
+                themeRef: {
+                    index: 9,
+                },
+            },
+            {
+                color: '#5e35b1',
+                seriesRef: {
+                    index: 6,
+                },
+            },
+            {
+                color: '#03a9f4',
+                seriesRef: {
+                    index: 7,
+                },
+            },
+            {
+                color: '#ec407a',
+                seriesRef: {
+                    index: 8,
+                },
+            },
+            {
+                color: '#ff7043',
+                seriesRef: {
+                    index: 9,
+                },
+            },
+            {
+                color: '#737373',
+                seriesRef: {
+                    index: 10,
+                },
+            },
+            {
+                color: '#f15a60',
+                seriesRef: {
+                    index: 11,
+                },
+            },
+            {
+                color: '#7ac36a',
+                seriesRef: {
+                    index: 12,
+                },
+            },
+            {
+                color: '#5a9bd4',
+                seriesRef: {
+                    index: 13,
+                },
+            },
+            {
+                color: '#faa75a',
+                seriesRef: {
+                    index: 14,
+                },
+            },
+            {
+                color: '#9e67ab',
+                seriesRef: {
+                    index: 15,
+                },
+            },
+            {
+                color: '#ce7058',
+                seriesRef: {
+                    index: 16,
+                },
+            },
+            {
+                color: '#d77fb3',
+                seriesRef: {
+                    index: 17,
+                },
+            },
+            {
+                color: '#81d4fa',
+                seriesRef: {
+                    index: 18,
+                },
+            },
+            {
+                color: '#f48fb1',
+                seriesRef: {
+                    index: 19,
+                },
+            },
+        ],
+        themeIncreaseColor: {
+            color: '#388e3c',
+        },
+        themeDecreaseColor: {
+            color: '#f44336',
+        },
+        themeGridColor: {
+            color: '#d1d1d1',
+        },
+    },
+    interactions: {},
+};
